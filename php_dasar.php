e0e54d1f57274253d02ed6e12ac4d75e64631cb7
KANGGA FAJARULHAKIM <anggabifik@gmail.com>
2024-11-15 23:12:50 +0700

    Create php_dasar.php

---
+++ php_dasar.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Belajar php</h1>
+    <?php
+        echo "Hello World !";
+    ?>
+    <h2>Menggunakan variabel</h2>
+    <?php
+    $nim = "312310430";
+    $nama = 'Kangga Fajarulhakim';
+    echo "NIM : " . $nim . "<br>";
+    echo "Nama : $nama";
+    ?>
+</body>
+</html>
